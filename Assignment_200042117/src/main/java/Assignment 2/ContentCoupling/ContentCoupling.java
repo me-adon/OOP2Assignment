bdfbb1ae76dc2d50c354c4cc347f7982c89e2e54
unknown <shadmanhabib@iut-dhaka.edu>
2022-11-21 22:07:15 +0600

    Content Coupling Done

---
+++ Assignment_200042117/src/main/java/Assignment 2/ContentCoupling/ContentCoupling.java
@@ -0,0 +1,11 @@
+package Assignment02.ContentCoupling;
+
+public class ContentCoupling  {
+
+    public  void usage (general General){
+        General.BMW();
+        General.Ferrari();
+        General.Porche();
+    }
+
+}
---
+++ Assignment_200042117/src/main/java/Assignment 2/ContentCoupling/General.java
@@ -0,0 +1,21 @@
+package Assignment02.ContentCoupling;
+
+public  class general {
+    int price;
+    String speed;
+
+    public void Ferrari(){
+        price = 2000000;
+        speed  = "120km/hr";
+    }
+
+    public void BMW(){
+        price = 5000000;
+        speed  = "150km/hr";
+    }
+    public void Porche(){
+        price = 4000000;
+        speed  = "180km/hr";
+    }
+
+}
